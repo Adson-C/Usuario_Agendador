6cbaeb3f5a0f31347c3aefcd2d273e0fa0fb078f
Adson Conceição <adsonconcecao@yahoo.com.br>
2024-11-16 19:21:30 -0300

    commit inicial

---
+++ src/main/java/com/adsonsa/useradson/UseradsonApplication.java
@@ -0,0 +1,13 @@
+package com.adsonsa.useradson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseradsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseradsonApplication.class, args);
+	}
+
+}
